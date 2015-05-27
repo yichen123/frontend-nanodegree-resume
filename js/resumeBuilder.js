418c5ba61ef2e7468dab78bafcb1e1d68e3b3bfc
yichen123 <chenyi330@hotmail.com>
2015-05-27 22:18:22 +0800

    clean up builder with four objects only

---
+++ js/resumeBuilder.js
@@ -0,0 +1,86 @@
+// $("#main").append("Chen YI");	
+var bio = {
+	"name": "Chen YI",
+	"role": "fresher web developer",
+	"contactInfo": 
+	{
+		"email": "cheny330@hotmail.com"
+	},
+	"pic": "images/fry.jpg",
+	"message": "Welcome to my site!",
+	"skills": 
+	[
+		"Maths", 
+		"Python", 
+		"Web Develop"
+	]
+}
+
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// var formattedcontact = HTMLcontactGeneric.replace("%data%", bio.contactInfo);
+// var formattedemail = HTMLemail.replace("%data%", bio.contactInfo.email);
+// var formattedbioPic = HTMLbioPic.replace("%data%", bio.pic);
+// var formattedmsg = HTMLwelcomeMsg.replace("%data%", bio.message);
+// var formattedskills = HTMLskills.replace("%data%", bio.skills);
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+
+var work = [
+	{
+		"position": "Board Assistant",
+		"employer": "BeijingKJG",
+		"years": 2,
+		"city": "Beijing"
+	}
+]
+
+var education = {
+	"schools": [
+		{
+			"school": "UON",
+			"years": "2012 - 2013",
+			"city": "Nottingham",
+			"major": "IB",
+			"graduate": "DEC 2013",
+			"degree": "Masters"
+		},
+		{
+			"school": "UON",
+			"years": "2010 - 2012",
+			"city": "Nottingham",
+			"major": "Maths",
+			"graduate": "2012",
+			"degree": "Masters"
+		}
+	],
+	"onlineCourses": [
+		{
+			"web": "coursera",
+			"course": "python"
+		},
+		{
+			"web": "udacity",
+			"course": "front end web developer" 
+		}
+
+	]
+}
+
+var projects = [
+	{
+		"title": "HomePage",
+		"date": "May 2015"
+	},
+	{
+		"title": "Resume",
+		"date": "unknown"
+	}
+]
+
+
+
+
+
